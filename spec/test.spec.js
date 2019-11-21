5f1a4368be6756de4f2c7a8d72d16a141d216a4c
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2019-11-21 14:35:33 +0200

    added part 2 functions

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,12 +1,14 @@
-var test_function=require('../src/basicCalculator.js');
+var calculator=require('../src/basicCalculator.js');
+
+let calculator_instance = new calculator;
    
 describe("function for adding",function() {       
        it("add two number and return", function(){
-        expect(test_function.add(0,0)).toEqual(0);
+        expect(calculator_instance.add(0,0)).toEqual(0);
        });
 
        it("function for adding many number", function(){
-        expect(test_function.add(1,2,3,4)).toEqual(10);
+        expect(calculator_instance.add(1,2,3,4)).toEqual(10);
         });
    });
 
@@ -14,11 +16,11 @@ describe("function for adding",function() {
 
   describe("function for multyply",function() {       
     it("multyply two number and return", function(){
-     expect(test_function.multiply(1,2)).toEqual(2);
+     expect(calculator_instance.multiply(1,2)).toEqual(2);
     });
 
     it("multyply two number and return", function(){
-        expect(test_function.multiply(1,2,3,4)).toEqual(24);
+        expect(calculator_instance.multiply(1,2,3,4)).toEqual(24);
        });
    });
       
@@ -26,6 +28,12 @@ describe("function for adding",function() {
     
 
 
+   console.log(calculator_instance.add(100,200));
      
+   console.log(calculator_instance.add("LAST", 5));
+   console.log(calculator_instance.set_slot(1));
+   console.log(calculator_instance.get_slot(1));
+   console.log(calculator_instance.add("SLOT_1", 5));
+   
     
 
--- src/basicCalculator.js
+++ src/basicCalculator.js
@@ -1,27 +1,103 @@
 
+module.exports = class calculator {
+    constructor() {
+        
+        this.last_value = 0;
+        this.slot = [];
+        //this.slotNumber = /[0-9]/;
+        
+        
+    }
+
+
+    add(){
+        let sum = 0;
+        for( var i=0; i < arguments.length; i++){
+            if(arguments[i] === "LAST"){
+                arguments[i] = this.last_value;
+               // sum = sum + arguments[i];
+            };
+            if(arguments[i] === "SLOT_1"){
+                arguments[i] = this.get_slot(1);
+
+            };
+
+            sum += (arguments[i]);
+            arguments[i] = this.last_value;
+            this.last_value = sum
+            
+
+            
+        
+        };
+        
+        return sum;
+    };
+
+     
+    multiply(){
+       var product = 1;
+        for( var i=0; i < arguments.length; i++){
+            if(arguments[i] === "LAST"){
+                arguments[i] = this.last_value;
+                //product = product * arguments[i];
+            };
+
+            if(arguments[i] === "SLOT_2"){
+                arguments[i] = this.get_slot(2);
+            };
+
+            product *= (arguments[i]);
+            arguments[i] = this.last_value;
+            this.last_value = product;
+            
+            
+        }return product;
+      
+        };
+
+
+        last(){
+
+            return this.last_value;
+        
+        };
+
+         
+        set_slot(slotNumber){
+            this.slot.push(this.last_value);
+           return this.slot[slotNumber - 1]
+        };
+
+
+
+        get_slot(slotNumber){
+
+            return this.slot[slotNumber - 1];
+
+        };
+        
+};
+
+
+ 
+
+
+
+
 
     
-function add(){
-    var sum = 0;
-    for( var i=0; i < arguments.length; i++){
-            sum = sum + arguments[i];
-        }return sum;
+// for testing 
+     
 
-}
+    
 
 
 
-function multiply(){
-    var product = 1;
-    for( var i=0; i < arguments.length; i++){
-            product = product * arguments[i];
-        }return product;
-
-}
 
 
 
-module.exports={
-    add,multiply
 
-}
+
+
+
