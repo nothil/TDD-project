3c0e24d4a9dc59b19fc509e63f19e09ba9806d90
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2019-11-22 13:27:10 +0200

    added specs

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -3,37 +3,48 @@ var calculator=require('../src/basicCalculator.js');
 let calculator_instance = new calculator;
    
 describe("function for adding",function() {       
-       it("add two number and return", function(){
-        expect(calculator_instance.add(0,0)).toEqual(0);
-       });
+   it("add two number and return", function(){
+      expect(calculator_instance.add(1,2)).toEqual(3);
 
-       it("function for adding many number", function(){
-        expect(calculator_instance.add(1,2,3,4)).toEqual(10);
-        });
-   });
+            });
+   it("function for adding many number", function(){
+      expect(calculator_instance.add(1,2,3,4)).toEqual(10);
+            });
 
-
-
-  describe("function for multyply",function() {       
-    it("multyply two number and return", function(){
-     expect(calculator_instance.multiply(1,2)).toEqual(2);
+   it("multyply two number and return", function(){
+      expect(calculator_instance.multiply(1,2,3,4)).toEqual(24);
     });
 
-    it("multyply two number and return", function(){
-        expect(calculator_instance.multiply(1,2,3,4)).toEqual(24);
-       });
-   });
+   it("multyply two number and return", function(){
+      expect(calculator_instance.multiply(1,2)).toEqual(2);
+         });
+
+         it("should give the sane answer for multiplication and get slot", function() {
+            expect(calculator_instance.multiply(1, 2)).toEqual(2);
+            expect(calculator_instance.get_slot());
+             });
       
+         
+       it('should take the last value and add 5', function() {
+        calculator_instance.map.set("SLOT_1", 3);
+         calculator_instance.multiply(1,3);
+           expect(calculator_instance.add( calculator_instance.map.get("MULTIPLY_SLOT")  ,5)).toEqual(8);
+
+      });
+
+   });
+
+        
+  
+         //testing all functions
+        console.log(calculator_instance.add(1,2,3,4));
+        console.log(calculator_instance.multiply(1,2));
+         
+        console.log(calculator_instance.add("LAST", 10));
+        console.log(calculator_instance.add("SLOT_1",5));
+        console.log(calculator_instance.multiply("SLOT_2", 2));
 
     
-
-
-   console.log(calculator_instance.add(100,200));
-     
-   console.log(calculator_instance.add("LAST", 5));
-   console.log(calculator_instance.set_slot(1));
-   console.log(calculator_instance.get_slot(1));
-   console.log(calculator_instance.add("SLOT_1", 5));
    
     
 
--- src/basicCalculator.js
+++ src/basicCalculator.js
@@ -3,16 +3,20 @@ module.exports = class calculator {
     constructor() {
         
         this.last_value = 0;
-        this.slot = [];
-        //this.slotNumber = /[0-9]/;
+        // this.slot = [];
+        this.slot =0;
         
+        this.map = new Map();
         
     }
 
 
     add(){
+       // console.log("starting...");
         let sum = 0;
+       // console.log("initialised sum ");
         for( var i=0; i < arguments.length; i++){
+            //console.log(" ....... " + i);
             if(arguments[i] === "LAST"){
                 arguments[i] = this.last_value;
                // sum = sum + arguments[i];
@@ -25,12 +29,12 @@ module.exports = class calculator {
             sum += (arguments[i]);
             arguments[i] = this.last_value;
             this.last_value = sum
-            
-
-            
+               
         
         };
         
+        this.set_slot(sum);
+        this.map.set("ADD_SLOT", sum);
         return sum;
     };
 
@@ -51,30 +55,31 @@ module.exports = class calculator {
             arguments[i] = this.last_value;
             this.last_value = product;
             
-            
+            this.set_slot(product);
+
+            this.map.set("MULTIPLY_SLOT", product);
+
+            // this.map.set("MULTILY_SLOT", product);
         }return product;
       
         };
 
 
-        last(){
 
+     last(){
+
+            
             return this.last_value;
-        
+
         };
 
          
         set_slot(slotNumber){
-            this.slot.push(this.last_value);
-           return this.slot[slotNumber - 1]
+            this.slot = slotNumber;
         };
 
-
-
-        get_slot(slotNumber){
-
-            return this.slot[slotNumber - 1];
-
+        get_slot(){
+            return this.slot;
         };
         
 };
@@ -87,7 +92,7 @@ module.exports = class calculator {
 
 
     
-// for testing 
+
      
 
     
